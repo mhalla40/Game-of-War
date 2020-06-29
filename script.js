c63df55c4f6e1d0a4d2c1ed2ae320ea78e2fda98
Michael Hall <mhalla40@gmail.com>
2020-06-28 20:49:50 -0400

    deck creation

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("hello world");
+//define player 1 and player 2//
+
+let player1 = {
+  name: "Player 1",
+  deck: [],
+};
+
+let player2 = {
+  name: "Player 2",
+  deck: [],
+};
+
+//Creating a card deck//
+
+let suits = ["Spade", "Club", "Diamond", "Heart"];
+let cards = [
+  "Two",
+  "Three",
+  "Four",
+  "Five",
+  "Six",
+  "Seven",
+  "Eight",
+  "Nine",
+  "Ten",
+  "Jack",
+  "Queen",
+  "King",
+  "Ace",
+];
+let deck = [];
